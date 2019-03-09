cb230c7af9a5e217278afc4734ced80fd5721c2d
Nareik <kieranquinn27@yahoo.ie>
2019-03-09 18:43:21 +0000

    Added images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Layout, Menu, Icon } from "antd";
+import MovieCard from "../src/containers/app/movieCard";
+import TopNav from "../src/containers/app/topNav";
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Layout>
+        <Header><TopNav /></Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>
+            <MovieCard />
+          </Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
     );
   }
 }
--- src/containers/app/movieCard.jsx
+++ src/containers/app/movieCard.jsx
@@ -1,10 +1,50 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Card, Icon } from "antd";
+
+const { Meta } = Card;
 
 class MovieCard extends Component {
   state = {};
-
+  settingsClick = () => {
+    alert("Settings Clicked");
+  };
+  editClick = () => {
+    alert("Edit Clicked");
+  };
+  moreClick = () => {
+    alert("More Clicked");
+  };
   render() {
-      
+    return (
+      <Card
+        hoverable
+        style={{ width: 300 }}
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={[
+          <a onClick={this.like}>
+            <Icon type="like" />
+          </a>,
+          <a onClick={this.settingsClick}>
+            <Icon type="setting" />
+          </a>,
+          <a onClick={this.editClick}>
+            <Icon type="edit" />
+          </a>,
+          <a onClick={this.moreClick}>
+            <Icon type="ellipsis" />
+          </a>
+        ]}
+      >
+        <Meta title="Card title" description="This is the description" />
+      </Card>
+    );
   }
 }
+
+export default MovieCard;
