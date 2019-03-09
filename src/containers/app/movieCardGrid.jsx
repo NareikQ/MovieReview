edbf218369c83bfffb1b4a79269ef8ae206b8df2
Nareik <kieranquinn27@yahoo.ie>
2019-03-09 19:25:42 +0000

    Updating grid

--- src/containers/app/movieCardGrid.jsx
+++ src/containers/app/movieCardGrid.jsx
@@ -5,75 +5,73 @@ import MovieCard from "../components/movieCard";
 
 const mainDiv = styled.div`
   padding: 24px;
+  .ant-col-6 {
+      padding-top: 24px;
+  }
 `;
+const data = [
+  {
+    image: "deadpool.jpg",
+    title: "Deadpool",
+    description: "(2015)"
+  },
+  {
+    image: "2007-transformers.jpg",
+    title: "Transformers",
+    description: "(2007)"
+  },
+  {
+    image: "BlackPanther.jpg",
+    title: "BlackPanther",
+    description: "(2017)"
+  },
+  {
+    image: "avengersInfinity.jpg",
+    title: "Avengers - Infinity War",
+    description: "(2018)"
+  },
+  {
+    image: "deadpool.jpg",
+    title: "Deadpool",
+    description: "(2015)"
+  },
+  {
+    image: "2007-transformers.jpg",
+    title: "Transformers",
+    description: "(2007)"
+  },
+  {
+    image: "BlackPanther.jpg",
+    title: "BlackPanther",
+    description: "(2017)"
+  },
+  {
+    image: "avengersInfinity.jpg",
+    title: "Avengers - Infinity War",
+    description: "(2018)"
+  }
+];
 
 class MovieCardGrid extends Component {
   state = {};
 
   render() {
+    const movies = data.map(function(movie, i) {
+      i++;
+
+        return (
+          <Col span={6}>
+            <MovieCard
+              image={movie.image}
+              title={movie.title}
+              description={movie.description}
+            />
+          </Col>
+        );
+    });
     return (
       <mainDiv>
-        <Row gutter={0}>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-        </Row>
-        <br />
-        <Row gutter={0}>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-          <Col span={6}>
-            <MovieCard
-              image="deadpool.jpg"
-              title="Deadpool"
-              description="(2016)"
-            />
-          </Col>
-        </Row>
+        <Row gutter={0}>{movies}</Row>
       </mainDiv>
     );
   }
